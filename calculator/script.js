039c960489a14ec67d074aa4236bf4de0a6aaea5
Sayan Ghosh <ghoshsayan894@gmail.com>
2021-12-20 20:47:25 +0530

    Update Calculator

--- calculator/script.js
+++ calculator/script.js
@@ -15,24 +15,13 @@ del.addEventListener('click',()=>{
 })
 const equal= document.querySelector(".equal");
 equal.addEventListener('click',()=>{
-    for(var i of text.innerHTML){
-        if("+-x/%".indexOf(i)!=-1)
-        var index = text.innerHTML.indexOf(i);
-    }
-    let len=text.innerHTML.length;
-    var num1 = parseInt(text.innerHTML.slice(0,index));
-    var num2 = parseInt(text.innerHTML.slice(index+1,len));
     var res;
-    if(text.innerHTML[index]==='+')
-    res = num1+num2;
-    if(text.innerHTML[index]==='-')
-    res = num1-num2;
-    if(text.innerHTML[index]==='x')
-    res = num1*num2;
-    if(text.innerHTML[index]==='/')
-    res = num1/num2;
-    if(text.innerHTML[index]==='%')
-    res = num1/100*num2;
-
+    try{
+        res = eval(text.innerHTML);
+    }
+    catch{
+        res = "Invalid Input";
+    }
     text.innerHTML = res;
+
 })
