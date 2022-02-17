9ee3acb5b339f2129acea74da07e65e363a5f9c7
Sayan Ghosh <ghoshsayan894@gmail.com>
2022-02-17 20:13:18 +0530

    Modified Weather App by getting Location

---
+++ Weather-By-City/readme.md
@@ -0,0 +1,7 @@
+It may happen that there is a little bit accuracy dispute,
+but it will not more than 2 deg,
+
+Hope You enjoy our weather app
+
+Special Thanks To Gairik Sharma for the CSS design,
+His github link - https://github.com/GairikSharma/
--- Weather-By-City/script.js
+++ Weather-By-City/script.js
@@ -5,6 +5,29 @@ var description = document.getElementsByTagName("h3")[0];
 var span = document.getElementsByTagName("span");
 var hum = document.getElementsByTagName("h5")[0];
 const button = document.getElementsByTagName("button")[0]
+
+window.addEventListener('load',()=>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((data)=>{
+            var latitude = data.coords.latitude;
+            var longitude = data.coords.longitude;
+            const url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+            fetch(url2).then(data=>{
+                return(data.json())
+            }).then((content)=>{
+                console.log(content);
+                var {main, weather,name}= content;
+                cityName.textContent = name;
+                description.textContent = weather[0].main
+                span[0].textContent = "Max: " + (parseFloat(main.temp_max)-273.15).toPrecision(4)+" °C"
+                span[1].textContent = "Min: " + (parseFloat(main.temp_min)-273.15).toPrecision(4)+" °C"
+                hum.textContent = "Humidity: " + main.humidity+"%"
+            })  
+        })
+    }}
+)
+
+
 button.addEventListener('click',()=>{
     city = document.getElementsByTagName("input")[0].value;
     document.getElementsByTagName("input")[0].value= '';
@@ -17,20 +40,19 @@ button.addEventListener('click',()=>{
     })
     .then(data => {
         try{
-        var {main, weather}= data;
-        cityName.textContent = city;
-        description.textContent = weather[0].main
-        span[0].textContent = "Max: " + (parseFloat(main.temp_max)-273.15).toPrecision(4)+" °C"
-        span[1].textContent = "Min: " + (parseFloat(main.temp_min)-273.15).toPrecision(4)+" °C"
-        hum.textContent = "Humidity: " + main.humidity+"%"
+            var {main, weather}= data;
+            cityName.textContent = city;
+            description.textContent = weather[0].main
+            span[0].textContent = "Max: " + (parseFloat(main.temp_max)-273.15).toPrecision(4)+" °C"
+            span[1].textContent = "Min: " + (parseFloat(main.temp_min)-273.15).toPrecision(4)+" °C"
+            hum.textContent = "Humidity: " + main.humidity+"%"
         }
         catch{
-        cityName.textContent = "Invalid Input";
-        description.textContent = ''
-        span[0].textContent = ''
-        span[1].textContent = ''
-        hum.textContent = ''
+            cityName.textContent = "Invalid Input";
+            description.textContent = ''
+            span[0].textContent = ''
+            span[1].textContent = ''
+            hum.textContent = ''
         }
     })
 })
-
